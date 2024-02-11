f0d1c5f882160f76246d53791f955b704616aef3
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-02-11 17:32:50 +0600

    existing user validation

--- Backend/controller/registerController.js
+++ Backend/controller/registerController.js
@@ -1,9 +1,19 @@
 const User = require("../model/userModel");
-const registerController = (req, res, next) => {
+const registerController = async (req, res, next) => {
   const { name, email, password, role } = req.body;
 
   if (!name || !email || !password) {
     return res.status(404).json({ error: "please fill the all field" });
+  }
+
+  if (password && password.length < 6) {
+    return res.status(400).json({ error: "password too small" });
+  }
+
+  // existing user checking
+  const existingUser = await User.find({ email: email });
+  if (existingUser.length > 0) {
+    return res.status(400).json({ error: "Email already existing" });
   }
   const userData = new User({
     name: name,
