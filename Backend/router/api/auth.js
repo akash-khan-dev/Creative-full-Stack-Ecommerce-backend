94ffcb3b96df83267d78a24271274942c192c0e6
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-04-04 15:14:36 +0600

    otp fun delete

--- Backend/router/api/auth.js
+++ Backend/router/api/auth.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const registerController = require("../../controller/registerController");
-const otpController = require("../../controller/optController");
 const loginController = require("../../controller/loginController");
 const linkController = require("../../controller/linkController");
 const resetMailController = require("../../controller/resetMailController");
@@ -9,7 +8,6 @@ const secureAPI = require("../../middleware/secureAPI");
 const newPasswords = require("../../controller/newPassword");
 
 router.post("/register", registerController);
-router.post("/otp", otpController);
 router.post("/linkVerified", linkController);
 router.post("/resetMail", resetMailController);
 router.post("/forgotpass", forgotPasswordController);
--- dashbord/src/App.jsx
+++ dashbord/src/App.jsx
@@ -1,7 +1,6 @@
 import EmailVerification from "./pages/EmailVerification/EmailVerification";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 import Login from "./pages/login/Login";
-import Otp from "./pages/otp/Otp";
 import Registration from "./pages/registration/Registration";
 import NewPassword from "./pages/NewPassword/NewPassword";
 import {
@@ -20,7 +19,6 @@ function App() {
         <Route path="/registration" index element={<Registration />} />
         <Route path="/forgotpass" index element={<ForgotPassword />} />
         <Route path="/newpass/:token" index element={<NewPassword />} />
-        <Route path="/otpVerification/:token" element={<Otp />} />
         <Route
           path="/emailVerification/:token"
           element={<EmailVerification />}
