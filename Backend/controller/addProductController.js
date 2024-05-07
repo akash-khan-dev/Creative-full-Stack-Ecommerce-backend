85cbddcbd7186bb0f9d98a96e4e88652e22dff57
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-05-07 13:53:56 +0600

    image upload backend comtomize

--- Backend/controller/addProductController.js
+++ Backend/controller/addProductController.js
@@ -1,15 +1,29 @@
 const ProductModel = require("../model/productModel");
-
+const multer = require("multer");
 const addProductController = (req, res, next) => {
   try {
-    const { name } = req.body;
-    const image = `/images/${req.file.filename}`;
-
-    const product = new ProductModel({
-      name: name,
-      image: image,
+    const { name = "hello" } = req.body;
+    const storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, "./images");
+      },
+      filename: function (req, file, cb) {
+        console.log(file);
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(null, uniqueSuffix + "-" + file.originalname);
+      },
     });
-    product.save();
+    const upload = multer({ storage: storage }).single("avatar");
+
+    upload(req, res, function (err) {
+      const image = req.file.path;
+      const product = new ProductModel({
+        name: name,
+        image: image,
+      });
+      product.save();
+    });
+
     return res
       .status(200)
       .json({ status: "success", message: "product added successfully" });
--- Backend/router/api/product.js
+++ Backend/router/api/product.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const multer = require("multer");
+
 const addCategoryController = require("../../controller/addCategoryController");
 const addSubCategoryController = require("../../controller/addSubCategoryController");
 const viewSubCategoryController = require("../../controller/viewSubCategoryController");
@@ -9,21 +9,11 @@ const verifyToken = require("../../middleware/verifyToken");
 const secureAPI = require("../../middleware/secureAPI");
 
 // image upload
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./images");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + "-" + file.originalname);
-  },
-});
 
-const upload = multer({ storage: storage });
 router.post("/addcategory", secureAPI, verifyToken, addCategoryController);
 router.post("/addsubcategory", addSubCategoryController);
 router.get("/viewcategory", viewCategoryController);
 router.get("/viewsubcategory", viewSubCategoryController);
-router.post("/addprodect", upload.single("avatar"), addProductController);
+router.post("/addprodect", addProductController);
 
 module.exports = router;
