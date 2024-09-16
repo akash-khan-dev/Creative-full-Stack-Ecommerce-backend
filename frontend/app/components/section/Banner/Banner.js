d50dda7e29fcdc80fd9eeccb5d58c213cddac263
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2024-09-16 23:52:13 +0600

    banner design

--- frontend/app/components/section/Banner/Banner.js
+++ frontend/app/components/section/Banner/Banner.js
@@ -1,24 +1,74 @@
 "use client";
+import "./banner.css";
 import Image from "next/image";
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-
-import Slider from "react-slick";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import hero from "../../../Image/hero image.png";
 import heroTwo from "../../../Image/01.png";
+import Link from "next/link";
 
 const Banner = () => {
   return (
-    <section id="banner">
+    <section id="banner" className="py-5">
       <Row>
         <Col lg={8}>
-          <div className="banner-img">
-            <Image src={hero} width={858} height={500} alt="hero" />
+          <Swiper
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Autoplay, Pagination, Navigation]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              {" "}
+              <div className="banner-img">
+                <Link href={"/"}>
+                  <Image src={hero} width={858} height={500} alt="hero" />
+                </Link>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <div className="banner-img">
+                <Link href={"/"}>
+                  <Image src={hero} width={858} height={500} alt="hero" />
+                </Link>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              {" "}
+              <div className="banner-img">
+                <Link href={"/"}>
+                  <Image src={hero} width={858} height={500} alt="hero" />
+                </Link>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </Col>
+        <Col lg={4}>
+          <div className="banner-add-wrapper">
+            <div className="add-img-wrapper">
+              <Link href={"/"}>
+                <Image src={heroTwo} width={415} height={250} alt="add" />
+              </Link>
+            </div>
+            <div className="add-img-wrapper">
+              <Link href={"/"}>
+                <Image src={heroTwo} width={415} height={250} alt="add" />
+              </Link>
+            </div>
+            <div></div>
           </div>
         </Col>
-
-        <Col lg={4}></Col>
       </Row>
     </section>
   );
